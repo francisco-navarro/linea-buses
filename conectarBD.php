f0a75adabed100dae8e2d9abc1414a6d1bcd5ad8
Paco Navarro <packarbell@gmail.com>
2019-04-30 18:57:18 +0200

    changes in files

--- conectarBD.php
+++ conectarBD.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $pwd = getenv('MY_PWD');
 
 @ $conexion = new mysqli('eu-cdbr-west-02.cleardb.net', 'bd20a27a0c1a32', $pwd, 'heroku_b74fe212de823a2');
--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<?
-header("Location: consulta.php");
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Repaso ENL</title>
+        <link rel="stylesheet" type="text/css" media="screen"
+            href="styles.css" />
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <header>
+            <h1>Base de datos de alumnos</h1>
+        </header>
+        <section>
+            <div class="contenido">
+                <h3>Consulta</h3>
+                <form>
+                    <label>Introduce Profesión</label><br>
+                    <input type="text" name="profesion" id="profesion" maxlength="9"
+                        autocomplete="off" autofocus/><br>
+                    <input type="submit" value="Consultar" class="submit"
+                        action="consultar();" /><br>
+                </form>
+            </div>
+            <div id="respuesta">
 
-?>
+            </div>
+        </section>
+    </body>
+</html>
