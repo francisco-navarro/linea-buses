d797a6055f9a83e0217ee1073b5457f928a3db02
Paco Navarro <packarbell@gmail.com>
2019-04-30 19:01:23 +0200

    modif consulta

--- consulta.php
+++ consulta.php
@@ -1,7 +1,4 @@
 <?
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 include 'conectarBD.php';
 
@@ -9,11 +6,16 @@ $profesion = $_GET['profesion'];
 
 $sql = "SELECT * FROM empleados WHERE profesion = '$profesion' ";
 
-$registros = array();
 
-while ($fila = $resul->fetch_assoc()) {
-    $registros[] = $fila;
+if ($resul = $mysqli->query($consulta)) {
+
+    $registros = array();
+    
+    while ($fila = $resul->fetch_assoc()) {
+        $registros[] = $fila;
+    }
 }
+
 
 echo Json_encode($registros);
 
